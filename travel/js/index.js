6434c1a4c0096b2e223751b59ca97ece3dbee19f
dementor <soumyadeep9@gmail.com>
2017-02-06 21:07:43 +0530

    Revert "First commit, some images and about me left."
    This reverts commit ea5516d5e375cda2bad00325606a2e01504fe091.

--- travel/js/index.js
+++ travel/js/index.js
@@ -6,12 +6,12 @@ $(function () {
         'np',  // Nepal
         'th',  // Thailand
         'sg',  // Singapore
-        //'my',  // Malaysia
-        //'ch',  // Switzerland
-        //'de',  // Germany
-        //'fr',  // France
-        //'au',   // Australia
-		'id',  //Indonesia
+        'my',  // Malaysia
+        'ch',  // Switzerland
+        'de',  // Germany
+        'fr',  // France
+        'au',   // Australia
+		
     ]
 
     const visited_states_in_india = [
